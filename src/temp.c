ef7e04968fe7094f1fcb8d3703cf01939015f47a
Dawn Kali <dawnkali@ev-q2.kzn.21-school.ru>
2024-01-11 17:21:58 +0300

    dawnkali: repair nirmic

--- src/temp.c
+++ src/temp.c
@@ -9,6 +9,7 @@ int main() {
   s21_decimal value_1 = {{1, 0, 0, 0}};
   s21_decimal value_2 = {{100, 0, 0, 0x00020000}};
   normalize_scale(&value_1, &value_2);
+  // mult_decimal_to_ten(&value_1);
   // mul_decimal_to_ten(&value_1);
   // mul_decimal_to_ten(&value_2);
   // div_decimal_to_ten(&value_1);
